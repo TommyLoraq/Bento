c1f002cbe0ee4ab1342013fb0b91c19e39ef7b7c
TommyLoraq <167106765+TommyLoraq@users.noreply.github.com>
2024-05-22 15:20:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -65,9 +65,9 @@ const CONFIG = {
 		},
 		{
 			id: '3',
-			name: 'Todoist',
-			icon: 'trello',
-			link: 'https://todoist.com',
+			name: 'Edu 21',
+			icon: 'panels-top-left',
+			link: 'https://edu.21-school.ru/',
 		},
 		{
 			id: '4',
